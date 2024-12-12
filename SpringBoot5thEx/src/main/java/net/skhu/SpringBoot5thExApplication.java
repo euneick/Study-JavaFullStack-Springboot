75e66fb523858f75c460c56236fb2969aeee0b28
euneick <study.euneick@gmail.com>
2024-12-12 12:37:45 +0900

    validation 어노테이션을 통해 입력된 값의 유효성 검사 // DTO 와 모델 클래스 구분

---
+++ SpringBoot5thEx/src/main/java/net/skhu/SpringBoot5thExApplication.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot5thExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot5thExApplication.class, args);
+	}
+
+}
---
+++ SpringBoot5thEx/src/main/java/net/skhu/controller/StudentController.java
@@ -0,0 +1,61 @@
+package net.skhu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import net.skhu.model.StudentEdit;
+
+@Controller
+@RequestMapping("student")
+@Slf4j
+public class StudentController {
+
+	@GetMapping("edit")
+	public String edit(Model model) {
+
+		StudentEdit studentEdit = new StudentEdit();
+		studentEdit.setName("이순신");
+
+		log.debug(studentEdit.toString());
+		model.addAttribute("studentEdit", studentEdit);
+
+		return "student/edit";
+	}
+
+	/*
+	 * 	BindingResult
+	 * 		@Valid 규칙을 검사하고 오류가 있으면, 그 정보를 얻는 인터페이스
+	 */
+	@PostMapping("edit")
+	public String edit(Model model, @Valid StudentEdit studentEdit, BindingResult bindingResult) {
+
+		try {
+			log.debug(studentEdit.toString());
+
+			if (bindingResult.hasErrors()) {
+				throw new Exception("입력한 형식이 올바르지 않습니다.");
+			}
+
+			return "redirect:list";
+		}
+		catch (Exception e) {
+			bindingResult.reject("", null, e.getMessage());
+			// 첫 번째 파라미터가 "" 이면 에러메시를 속성이 아닌 StudentEdit 객체 자체에 등록
+
+			return "student/edit";
+		}
+	}
+
+	@GetMapping("list")
+	public String list() {
+		return "student/list";
+	}
+
+
+}
